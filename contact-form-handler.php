362edfeb6772455d712d3ffa000b805077291e1f
Haci Karahasanoglu <haci@hacikho.com>
2019-05-16 10:59:00 -0400

    modification

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -10,7 +10,7 @@ if(empty($_POST['name'])  ||
 
 $name = $_POST['name']; 
 $email_address = $_POST['email']; 
-$message = $_POST['message']; 
+$messagee = $_POST['message']; 
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
@@ -19,20 +19,47 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
-if( empty($errors))
-{
-	$to = $myemail; 
-	$email_subject = "Contact form submission: $name";
-	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
-	
-	$headers = "From: $myemail\n"; 
-	$headers .= "Reply-To: $email_address";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
-} 
+
+// Login credentials
+ $username = 'azure_a9fc619a86fcedf097b225e5b5559574@azure.com';
+ $password = ')ud7fchX!@';
+
+ // Setup Swift mailer parameters
+ $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
+ $transport->setUsername($username);
+ $transport->setPassword($password);
+ $swift = Swift_Mailer::newInstance($transport);
+
+
+
+$email_subject = "Contact form submission: $name";
+ // Create a message (subject)
+ $message = new Swift_Message($email_subject);
+
+ // attach the body of the email
+ $message->setFrom($from);
+
+ $email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $messagee"; 
+ $message->setBody($email_body);
+ 
+ $to = $myemail;
+ $message->setTo($to);
+ 
+
+
+ // send message
+ if ($recipients = $swift->send($message, $failures))
+ {
+     // This will let us know how many users received this message
+     echo 'Message sent out to '.$recipients.' users';
+ }
+ // something went wrong =(
+ else
+ {
+     echo "Something went wrong - ";
+     print_r($failures);
+ }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
